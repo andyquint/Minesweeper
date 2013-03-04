ac592a6adbe1b176da26df6d944bbf78cf882456
Andrew Quintanilla <quint.andrew@gmail.com>
2013-03-04 15:42:14 -0800

    Added tile flagging
    Right-clicking flags tiles.
    Still need to add win condition (game is won when all mines are
    flagged).
    Thinking about adding time display. May have random number of mines
    (within a bound) instead of preset number of mines. Should add
    autochecking of tiles surrounding a 0 tile.

--- minesweeper.py
+++ minesweeper.py
@@ -8,6 +8,7 @@ white=(255,255,255)
 black=(0,0,0)
 grey=(169,169,169)
 red=(255,0,0)
+orange=(255,165,0)
 size = 20
 pygame.init()
 font = pygame.font.SysFont("calibri", 14)
@@ -26,9 +27,10 @@ class Tile(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.checked = False
 		self.isBomb = False
+		self.flagged = False
 		self.num = 0
 		
-	def check(self):
+	def Check(self):
 		if (self.isBomb):
 			self.inside.fill(red)
 		else:
@@ -40,6 +42,16 @@ class Tile(pygame.sprite.Sprite):
 				
 		self.image.blit(self.inside,(2,2))
 		self.checked = True
+		
+	def Flag(self):
+		if self.flagged:
+			self.inside.fill(grey)
+			self.image.blit(self.inside,(2,2))
+		else:
+			flag = pygame.Surface((self.inside.get_width()-4,self.inside.get_height()-4))
+			flag.fill(orange)
+			self.inside.blit(flag,(2,2))
+			self.image.blit(self.inside,(2,2))
 
 class Grid(pygame.sprite.Group):
 	def __init__(self):
@@ -71,7 +83,8 @@ class Grid(pygame.sprite.Group):
 def main():
 	grid = Grid()
 	screen = pygame.display.set_mode((grid.dimensions[0]*size+2,grid.dimensions[1]*size+2))
-	buttonpress = False
+	lbuttonpress = False
+	rbuttonpress = False
 	background = pygame.Surface(screen.get_size())
 	background = background.convert()
 	background.fill(white)
@@ -83,18 +96,29 @@ def main():
 			if event.type == QUIT:
 				return
 			elif event.type == MOUSEBUTTONDOWN:
-				if buttonpress == False:
-					buttonpress = True
+				if pygame.mouse.get_pressed()[0] and not lbuttonpress:
+					lbuttonpress = True
+				if pygame.mouse.get_pressed()[2] and not rbuttonpress:
+					rbuttonpress = True
 			elif event.type == MOUSEBUTTONUP:
-				if buttonpress == True:
-					buttonpress = False
-					
+				if not pygame.mouse.get_pressed()[0] and lbuttonpress:
+					print('lclick')
+					lbuttonpress = False
 					mousesprite = pygame.sprite.Sprite()
 					mousesprite.rect = pygame.Rect(pygame.mouse.get_pos(), (1,1))
 					intersect = pygame.sprite.spritecollide(mousesprite,grid,False)
 					if intersect:
 						for i in intersect:
-							i.check()
+							i.Check()
+				if not pygame.mouse.get_pressed()[2] and rbuttonpress:
+					print('rclick')
+					rbuttonpress = False
+					mousesprite = pygame.sprite.Sprite()
+					mousesprite.rect = pygame.Rect(pygame.mouse.get_pos(), (1,1))
+					intersect = pygame.sprite.spritecollide(mousesprite,grid,False)
+					if intersect:
+						for i in intersect:
+							i.Flag()
 		
 		grid.draw(screen)
 		pygame.display.flip()
