31e9f5a0882c87b8d19e0eef2b1a58a3d58281a0
Andrew Quintanilla <quint.andrew@gmail.com>
2013-03-04 16:43:00 -0800

    Added autochecking around blank tiles
    Introduced performance issues from doing unnecessary checks. Can fix by
    separating checked and unchecked tiles into separate groups.

--- minesweeper.py
+++ minesweeper.py
@@ -31,33 +31,37 @@ class Tile(pygame.sprite.Sprite):
 		self.num = 0
 		
 	def Check(self):
-		if (self.isBomb):
-			self.inside.fill(red)
-		else:
-			self.inside.fill(white)
-			print(self.num)
-			if (self.num != 0):
-				numtext = font.render(str(self.num), True, black)
-				self.inside.blit(numtext,(self.rect.width/4,numtext.get_height()/4))
-				
-		self.image.blit(self.inside,(2,2))
-		self.checked = True
+		if not self.checked:
+			if (self.isBomb):
+				self.inside.fill(red)
+			else:
+				self.inside.fill(white)
+				print(self.num)
+				if (self.num != 0):
+					numtext = font.render(str(self.num), True, black)
+					self.inside.blit(numtext,(self.rect.width/4,numtext.get_height()/4))
+					
+			self.image.blit(self.inside,(2,2))
+			self.checked = True
 		
 	def Flag(self):
+	# Returns True if a tile is being flagged, False if a tile is being unflagged
 		if self.flagged:
 			self.inside.fill(grey)
 			self.image.blit(self.inside,(2,2))
+			return False
 		else:
 			flag = pygame.Surface((self.inside.get_width()-4,self.inside.get_height()-4))
 			flag.fill(orange)
 			self.inside.blit(flag,(2,2))
 			self.image.blit(self.inside,(2,2))
+			return True
 
 class Grid(pygame.sprite.Group):
 	def __init__(self):
 		pygame.sprite.Group.__init__(self)
 		self.dimensions = (16,16)
-		self.bombs = 60
+		self.bombs = 20
 		test = list(range(self.dimensions[0]*self.dimensions[1]))
 		shuffle(test)
 		for i in range(self.dimensions[0]):
@@ -81,8 +85,12 @@ class Grid(pygame.sprite.Group):
 						t.num = t.num+1
 				
 def main():
+	# These will need to be reinitialized for a new game
 	grid = Grid()
-	screen = pygame.display.set_mode((grid.dimensions[0]*size+2,grid.dimensions[1]*size+2))
+	flags = 0
+	#
+	
+	screen = pygame.display.set_mode((grid.dimensions[0]*size+2,grid.dimensions[1]*size+2+30))
 	lbuttonpress = False
 	rbuttonpress = False
 	background = pygame.Surface(screen.get_size())
@@ -106,10 +114,18 @@ def main():
 					lbuttonpress = False
 					mousesprite = pygame.sprite.Sprite()
 					mousesprite.rect = pygame.Rect(pygame.mouse.get_pos(), (1,1))
-					intersect = pygame.sprite.spritecollide(mousesprite,grid,False)
-					if intersect:
-						for i in intersect:
-							i.Check()
+					mintersect = pygame.sprite.spritecollide(mousesprite,grid,False)
+					if mintersect:
+						for m in mintersect:
+							m.Check()
+							if not m.isBomb and m.num == 0:
+								temp = pygame.sprite.Sprite()
+								temp.rect = m.rect.copy()
+								temp.rect.inflate_ip(2,2)
+								rintersect = pygame.sprite.spritecollide(temp,grid,False)
+								for r in rintersect:
+									if not r.checked:
+										mintersect.append(r)
 				if not pygame.mouse.get_pressed()[2] and rbuttonpress:
 					print('rclick')
 					rbuttonpress = False
@@ -118,7 +134,10 @@ def main():
 					intersect = pygame.sprite.spritecollide(mousesprite,grid,False)
 					if intersect:
 						for i in intersect:
-							i.Flag()
+							if i.Flag():
+								flags = flags+1
+							else:
+								flags = flags-1
 		
 		grid.draw(screen)
 		pygame.display.flip()
